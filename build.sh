3dfe75c7432e5e58c6f73de0c605882e4c2e2868
Andrey Utkin <andrey.krieger.utkin@gmail.com>
2011-12-21 17:59:23 +0200

    Enhance build script

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #gengetopt -i imgs2video.ggo -a args -F imgs2video_cmdline
-gcc imgs2video.c imgs2video_cmdline.c -lavformat -lavfilter -o imgs2video -g -ggdb
-gcc cat.c -o cat -lavformat -g -ggdb
+
+# with CFLAG -I and LDFLAG -L you can set custom path to ffmpeg stuff
+# and with export'ing LD_LIBRARY_PATH, to use it at runtime
+CFLAGS="-g -ggdb"
+LDFLAGS="-lavformat -lavfilter"
+gcc imgs2video.c imgs2video_cmdline.c -o imgs2video $CFLAGS $LDFLAGS
+gcc cat.c -o cat $CFLAGS $LDFLAGS
