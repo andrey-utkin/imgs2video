a85942345f2fb1b2b4814b11b647f629a8aff3da
Andrey Utkin <andrey.krieger.utkin@gmail.com>
2013-11-18 18:45:42 +0200

    Fix mtime setting at remove_old.sh

--- remove_old.sh
+++ remove_old.sh
@@ -12,7 +12,18 @@ then
 fi
 source $IMGS2VIDEO_CFGFILE
 
-find $IMGSDIR/* -mtime +$SAVE_IMGS_DAYS -exec rm -rf {} \; &>/dev/null
-find $VIDEODIR/* -mtime +$SAVE_VIDEO_HOURS_DAYS -exec rm -rf {} \; &>/dev/null
-find $DAILY_VIDEO_DIR/* -mtime +$SAVE_VIDEO_DAYS_DAYS -exec rm -rf {} \; &>/dev/null
-find $LOG_DIR/* -mtime +$SAVE_LOG_DAYS -exec rm -rf {} \; &>/dev/null
+IMGSDIR_MTIME=$(( SAVE_IMGS_DAYS - 1 ))
+[[ IMGSDIR_MTIME -lt 0 ]] && IMGSDIR_MTIME=0  # avoid negative values
+find $IMGSDIR/* -mtime +$IMGSDIR_MTIME -exec rm -rf {} \; &>/dev/null
+
+VIDEODIR_MTIME=$(( SAVE_VIDEO_HOURS_DAYS - 1 ))
+[[ VIDEODIR_MTIME -lt 0 ]] && VIDEODIR_MTIME=0  # avoid negative values
+find $VIDEODIR/* -mtime +$VIDEODIR_MTIME -exec rm -rf {} \; &>/dev/null
+
+DAILY_VIDEO_DIR_MTIME=$(( SAVE_VIDEO_DAYS_DAYS - 1 ))
+[[ DAILY_VIDEO_DIR_MTIME -lt 0 ]] && DAILY_VIDEO_DIR_MTIME=0  # avoid negative values
+find $DAILY_VIDEO_DIR/* -mtime +$DAILY_VIDEO_DIR_MTIME -exec rm -rf {} \; &>/dev/null
+
+LOG_DIR_MTIME=$(( SAVE_LOG_DAYS - 1 ))
+[[ LOG_DIR_MTIME -lt 0 ]] && LOG_DIR_MTIME=0  # avoid negative values
+find $LOG_DIR/* -mtime +$LOG_DIR_MTIME -exec rm -rf {} \; &>/dev/null
